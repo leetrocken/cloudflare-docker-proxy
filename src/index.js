8147e932fbddceef4edeab3138a8cfe5344d8a59
leetrocken <66830656+leetrocken@users.noreply.github.com>
2024-09-05 08:43:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.liluo.space": dockerHub,
+  "quay.liluo.space": "https://quay.io",
+  "gcr.liluo.space": "https://gcr.io",
+  "k8s-gcr.liluo.space": "https://k8s.gcr.io",
+  "k8s.liluo.space": "https://registry.k8s.io",
+  "ghcr.liluo.space": "https://ghcr.io",
+  "cloudsmith.liluo.space": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.liluo.space": dockerHub,
 };
 
 function routeByHosts(host) {
